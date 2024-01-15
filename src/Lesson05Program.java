d196eba5056c6adc4d1366e2d0cbb21c24c9b8fe
Yuliya Alesik <alesikyuliya@outlook.com>
2024-01-15 15:27:20 +0100

    First commit

---
+++ src/Lesson05Program.java
@@ -0,0 +1,62 @@
+public class Lesson05Program {
+
+    public static void main(String[] args) {
+
+        boolean bool = false;
+        int a = 0;
+        while (!bool){
+            System.out.println(a);
+            a++;
+            if(a == 10){
+                bool = true;
+
+            }
+        }
+    }
+    }
+//        int a = 0;
+//
+//        while (a < 10){
+//            System.out.println(a);
+//            a++;
+//        }
+//
+//    }
+//}
+
+
+
+
+
+
+        //for (int i = 0; i <= 10; i++){
+            //if(i % 2 == 1) {
+               // System.out.println(i);
+
+
+
+        //int i = 0;
+        //for (; i < 5; i++){
+            //System.out.println(i);
+
+
+        //System.out.println("____________");
+        //System.out.println(i);
+
+
+        //for (int i = 0; i < 5; i++){
+            //System.out.println(i);
+
+        //int a = 3;
+        //for (int i = 0; i <5; i++){
+            //System.out.println(a);
+            //a++;
+
+
+
+        //int a = 3;
+        //System.out.println(a);
+        //a++;
+        //System.out.println(a);
+
+
