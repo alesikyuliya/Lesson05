f6b7c96311aaac860fbc21dd175f58f6b66db313
Yuliya Alesik <alesikyuliya@outlook.com>
2024-01-17 15:26:15 +0100

    Third commit

--- src/Lesson05Program.java
+++ src/Lesson05Program.java
@@ -1,29 +1,103 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Lesson05Program {
 
     public static void main(String[] args) {
 
-        int a = 0;
+        Random random = new Random();
 
-        while(true){
-            System.out.println(a);
-            a++;
-            if(a > 10){
-                break;
-            }
+        int[] arrayInt = new int[25];
+        int keyNumber = 33;
+
+        for (int index = 0; index < arrayInt.length; index++) {
+            arrayInt[index] = random.nextInt(50) + 1;
+
         }
 
-//        for (; ;){
-//            System.out.println(a);
-//            a++;
-//            if (a > 10){
-//                return;
+        System.out.println(Arrays.toString(arrayInt));
+
+        for (int index = 0; index < arrayInt.length; index++) {
+            if (arrayInt[index] == keyNumber) {
+                System.out.println(arrayInt[index] + " with index = " + index + " ");
+            }
+
+        }
+    }
+}
+
+//        for (int arrayElement:
+//        arrayInt){
+//            if (arrayElement == keyNumber){
+//                System.out.println(arrayElement);
 //            }
-//
 //        }
 
+//        int[] manualArrayInt = new int[]{10, 5, 1, 23, 16, -4, -15};
+//        System.out.println(Arrays.toString(manualArrayInt));
+//        Arrays.sort(manualArrayInt);
+//        System.out.println(Arrays.toString(manualArrayInt));
 
-    }
-    }
+
+
+//
+//        int[] manualArrayInt = new int[]{19, 2, 8, 1, 4, 10, 3};
+//        int[] manualArrayInt2 = {19, 2, 8, 1, 4, 10, 3};
+//
+//        System.out.println(Arrays.toString(manualArrayInt));
+//        System.out.println(Arrays.toString(manualArrayInt2));
+
+//        System.out.println(manualArrayInt.length);
+//        for (int arrayElement:
+//        manualArrayInt){
+//            System.out.print(arrayElement + ", ");
+//        }
+//        System.out.println();
+//        System.out.println(Arrays.toString(manualArrayInt));
+
+
+
+
+
+
+//        int[] arrayInt = new int[5];
+//
+//        for (int index = 0; index < arrayInt.length; index++) {
+//            arrayInt[index] = random.nextInt(10) +1;
+//            System.out.print(arrayInt[index] + " (" + index + ")  ");
+//        }
+//        System.out.println();
+//        System.out.println(Arrays.toString(arrayInt));
+//
+//    }
+//}
+
+
+//        int a = 0;
+//
+//        while(true){
+//            System.out.println(a);
+//            a++;
+//            if(a > 10){
+//                break;
+//            }
+//        }
+//
+////        for (; ;){
+////            System.out.println(a);
+////            a++;
+////            if (a > 10){
+////                return;
+////            }
+////
+////        }
+//
+//
+//    }
+//    }
 //        int a = 0;
 //
 //        do {
@@ -56,38 +130,32 @@ public class Lesson05Program {
 //}
 
 
+//for (int i = 0; i <= 10; i++){
+//if(i % 2 == 1) {
+// System.out.println(i);
 
 
+//int i = 0;
+//for (; i < 5; i++){
+//System.out.println(i);
 
 
-        //for (int i = 0; i <= 10; i++){
-            //if(i % 2 == 1) {
-               // System.out.println(i);
+//System.out.println("____________");
+//System.out.println(i);
 
 
+//for (int i = 0; i < 5; i++){
+//System.out.println(i);
 
-        //int i = 0;
-        //for (; i < 5; i++){
-            //System.out.println(i);
-
-
-        //System.out.println("____________");
-        //System.out.println(i);
-
-
-        //for (int i = 0; i < 5; i++){
-            //System.out.println(i);
-
-        //int a = 3;
-        //for (int i = 0; i <5; i++){
-            //System.out.println(a);
-            //a++;
+//int a = 3;
+//for (int i = 0; i <5; i++){
+//System.out.println(a);
+//a++;
 
 
-
-        //int a = 3;
-        //System.out.println(a);
-        //a++;
-        //System.out.println(a);
+//int a = 3;
+//System.out.println(a);
+//a++;
+//System.out.println(a);
 
 
